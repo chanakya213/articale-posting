0fab49033eaf549ba04632ef16dfdc4e2e7ccd24
John Doe <chanakya213@email.com>
2022-06-01 18:38:12 +0530

    some validating added

--- src/App.jsx
+++ src/App.jsx
@@ -3,57 +3,69 @@ import Nav from "./componets/Nav";
 import Textarea from "./componets/Textarea";
 import Insert from "./componets/Insert";
 
-function App(){
-  let [tempValue,handletemp] = useState({ title:"" , content:"" });
+function App() {
+  let [tempValue, handletemp] = useState({ title: "", content: "" });
 
-  let [finalValue,handlevalue] = useState([]);
+  let [finalValue, handlevalue] = useState([]);
 
-  function hadlechange(evt){
-      let inputdata = evt.target.value;
-      let name = evt.target.name;
+  function hadlechange(evt) {
+    let inputdata = evt.target.value;
+    let name = evt.target.name;
 
-      handletemp(prev=>{
-          if(name == "title"){
-              return {
-                  ...prev,
-                  title:inputdata
-              }
-          }else if(name == "content"){
-              return {
-                  ...prev,
-                  content:inputdata
-              }
-          }
-      })
-  };
+    handletemp((prev) => {
+      if (name == "title") {
+        return {
+          ...prev,
+          title: inputdata,
+        };
+      } else if (name == "content") {
+        return {
+          ...prev,
+          content: inputdata,
+        };
+      }
+    });
+  }
 
-
-function adding(){
-  handlevalue(prev=>{
-    //used spread operator to hold the prev values of an array
-    return [...prev,
-    tempValue]
-  })
-  handletemp("");
-};
-function deleteitem(id){
-   handlevalue((prev)=>{
-    return prev.filter((x,index)=>{
-           return id !== index;    
-    })
-  })
+  function adding() {
+    if (tempValue.title.length !== 0) {
+      handlevalue((prev) => {
+        //used spread operator to hold the prev values of an array
+        return [...prev, tempValue];
+      });
+      handletemp({ title: "", content: "" });
+    } else {
+      alert("data is empty pl write something");
+    }
+  }
+  function deleteitem(id) {
+    handlevalue((prev) => {
+      return prev.filter((x, index) => {
+        return id !== index;
+      });
+    });
+  }
+  return (
+    <div>
+      <Nav />
+      <div className="inputarea">
+        <Insert hadlechange={hadlechange} adding={adding} />
+      </div>
+      <div className="container">
+        {finalValue.map((x, index) => {
+          return (
+            <Textarea
+              key={index}
+              id={index}
+              title={x.title}
+              content={x.content}
+              deleteitem={deleteitem}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
-  return ( <div>
-       <Nav />
-       <div className="inputarea">
-       <Insert hadlechange={hadlechange} adding={adding} />
-       </div>
-       <div className="container">
-         {finalValue.map((x,index)=>{
-           return <Textarea key={index} id={index} title={x.title} content={x.content} deleteitem={deleteitem}/>
-         })}
-       </div>
-    </div>)
-};
 
 export default App;
--- src/componets/Textarea.jsx
+++ src/componets/Textarea.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 
-function Textarea(props){
-    return (
-        <div className="textarea">
-              <h3>{props.title}</h3>
-              <p>{props.content}</p>
-              <a href="#" onClick={ ()=>{
-                   props.deleteitem(props.id)
-              }}>del</a>
-        </div>
-    )
-};
+function Textarea(props) {
+  return (
+    <div className="textarea">
+      <h3>{props.title}</h3>
+      <p>{props.content}</p>
+      <a
+        href="#"
+        onClick={() => {
+          props.deleteitem(props.id);
+        }}
+      >
+        Del
+      </a>
+    </div>
+  );
+}
 
 export default Textarea;
